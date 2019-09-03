dfcacd672b305794fcbb91650f4a782dd98be1ad
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2019-09-03 18:49:16 -0300

    Clases

---
+++ Primaton-G3-Reciclaje/Primaton-G3-Reciclaje/Back-End/Clases/Contenedor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Primaton_G3_Reciclaje.Back_End.Clases
+{
+    class Contenedor
+    {
+    }
+}
