71b529ead185f4d221583a158f4e41d0ee5b5709
grafito <fabriivann@gmail.com>
2019-09-09 01:37:49 -0300

    Modificando form vidrio
    cambio completo diseño de boceto.

--- Primaton-G3-Reciclaje/Primaton-G3-Reciclaje/Front-End/Formularios/frm_vidrio.Designer.cs
+++ Primaton-G3-Reciclaje/Primaton-G3-Reciclaje/Front-End/Formularios/frm_vidrio.Designer.cs
@@ -28,164 +28,95 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.txtVidrio = new System.Windows.Forms.TextBox();
-            this.picPeso1 = new System.Windows.Forms.PictureBox();
-            this.picPeso2 = new System.Windows.Forms.PictureBox();
-            this.picPeso3 = new System.Windows.Forms.PictureBox();
-            this.txtPeso1 = new System.Windows.Forms.TextBox();
-            this.txtPeso2 = new System.Windows.Forms.TextBox();
-            this.txtPeso3 = new System.Windows.Forms.TextBox();
-            this.txtGramos1 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.btn100gramos = new System.Windows.Forms.Button();
-            this.btn200gramos = new System.Windows.Forms.Button();
-            this.txtGramos2 = new System.Windows.Forms.Label();
-            this.txtGramos3 = new System.Windows.Forms.Label();
-            this.btn5gramos = new System.Windows.Forms.Button();
-            ((System.ComponentModel.ISupportInitialize)(this.picPeso1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picPeso2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picPeso3)).BeginInit();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.txtinformacion = new System.Windows.Forms.Label();
+            this.btn_Pesar = new System.Windows.Forms.Button();
+            this.btn_siguiente = new System.Windows.Forms.Button();
+            this.btn_salir = new System.Windows.Forms.Button();
+            this.tiempofecha = new System.Windows.Forms.Timer(this.components);
+            this.txtFecha = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txtVidrio
             // 
             this.txtVidrio.BackColor = System.Drawing.Color.MediumTurquoise;
-            this.txtVidrio.Font = new System.Drawing.Font("Verdana", 8.25F);
-            this.txtVidrio.Location = new System.Drawing.Point(288, 24);
+            this.txtVidrio.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtVidrio.Font = new System.Drawing.Font("Verdana", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtVidrio.Location = new System.Drawing.Point(25, 12);
+            this.txtVidrio.Multiline = true;
             this.txtVidrio.Name = "txtVidrio";
-            this.txtVidrio.Size = new System.Drawing.Size(100, 21);
+            this.txtVidrio.ShortcutsEnabled = false;
+            this.txtVidrio.Size = new System.Drawing.Size(252, 25);
             this.txtVidrio.TabIndex = 0;
-            this.txtVidrio.Text = "VIDRIO";
+            this.txtVidrio.Text = "SELECCIONÓ VIDRIO";
             this.txtVidrio.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             // 
-            // picPeso1
+            // txtInfo
             // 
-            this.picPeso1.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.picPeso1.Location = new System.Drawing.Point(12, 130);
-            this.picPeso1.Name = "picPeso1";
-            this.picPeso1.Size = new System.Drawing.Size(144, 103);
-            this.picPeso1.TabIndex = 1;
-            this.picPeso1.TabStop = false;
+            this.txtInfo.BackColor = System.Drawing.Color.DarkGray;
+            this.txtInfo.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.txtInfo.Location = new System.Drawing.Point(52, 60);
+            this.txtInfo.Multiline = true;
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(701, 397);
+            this.txtInfo.TabIndex = 1;
             // 
-            // picPeso2
+            // txtinformacion
             // 
-            this.picPeso2.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.picPeso2.Location = new System.Drawing.Point(165, 130);
-            this.picPeso2.Name = "picPeso2";
-            this.picPeso2.Size = new System.Drawing.Size(144, 103);
-            this.picPeso2.TabIndex = 2;
-            this.picPeso2.TabStop = false;
+            this.txtinformacion.BackColor = System.Drawing.Color.Transparent;
+            this.txtinformacion.Font = new System.Drawing.Font("Verdana", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtinformacion.Location = new System.Drawing.Point(56, 111);
+            this.txtinformacion.Name = "txtinformacion";
+            this.txtinformacion.Size = new System.Drawing.Size(656, 218);
+            this.txtinformacion.TabIndex = 2;
+            this.txtinformacion.Text = "Poner todo dentro del contenedor y esperar unos segundos para el pesaje, cuando i" +
+    "ntroduzca todo el contenido presione el botón \"PESAR\"";
             // 
-            // picPeso3
+            // btn_Pesar
             // 
-            this.picPeso3.BackColor = System.Drawing.SystemColors.InactiveCaption;
-            this.picPeso3.Location = new System.Drawing.Point(315, 130);
-            this.picPeso3.Name = "picPeso3";
-            this.picPeso3.Size = new System.Drawing.Size(144, 103);
-            this.picPeso3.TabIndex = 3;
-            this.picPeso3.TabStop = false;
+            this.btn_Pesar.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_Pesar.Location = new System.Drawing.Point(321, 332);
+            this.btn_Pesar.Name = "btn_Pesar";
+            this.btn_Pesar.Size = new System.Drawing.Size(135, 51);
+            this.btn_Pesar.TabIndex = 3;
+            this.btn_Pesar.Text = "PESAR";
+            this.btn_Pesar.UseVisualStyleBackColor = true;
             // 
-            // txtPeso1
+            // btn_siguiente
             // 
-            this.txtPeso1.Font = new System.Drawing.Font("Verdana", 8.25F);
-            this.txtPeso1.Location = new System.Drawing.Point(40, 249);
-            this.txtPeso1.Name = "txtPeso1";
-            this.txtPeso1.Size = new System.Drawing.Size(69, 21);
-            this.txtPeso1.TabIndex = 4;
-            this.txtPeso1.Text = "50 P";
-            this.txtPeso1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.btn_siguiente.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_siguiente.Location = new System.Drawing.Point(162, 408);
+            this.btn_siguiente.Name = "btn_siguiente";
+            this.btn_siguiente.Size = new System.Drawing.Size(164, 38);
+            this.btn_siguiente.TabIndex = 4;
+            this.btn_siguiente.Text = "SIGUIENTE";
+            this.btn_siguiente.UseVisualStyleBackColor = true;
             // 
-            // txtPeso2
+            // btn_salir
             // 
-            this.txtPeso2.Font = new System.Drawing.Font("Verdana", 8.25F);
-            this.txtPeso2.Location = new System.Drawing.Point(198, 250);
-            this.txtPeso2.Multiline = true;
-            this.txtPeso2.Name = "txtPeso2";
-            this.txtPeso2.Size = new System.Drawing.Size(66, 20);
-            this.txtPeso2.TabIndex = 5;
-            this.txtPeso2.Text = "100 P";
-            this.txtPeso2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
-            this.txtPeso2.TextChanged += new System.EventHandler(this.TxtPeso2_TextChanged);
+            this.btn_salir.Font = new System.Drawing.Font("Verdana", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_salir.Location = new System.Drawing.Point(444, 408);
+            this.btn_salir.Name = "btn_salir";
+            this.btn_salir.Size = new System.Drawing.Size(164, 38);
+            this.btn_salir.TabIndex = 5;
+            this.btn_salir.Text = "SALIR";
+            this.btn_salir.UseVisualStyleBackColor = true;
             // 
-            // txtPeso3
+            // tiempofecha
             // 
-            this.txtPeso3.Font = new System.Drawing.Font("Verdana", 8.25F);
-            this.txtPeso3.Location = new System.Drawing.Point(343, 250);
-            this.txtPeso3.Multiline = true;
-            this.txtPeso3.Name = "txtPeso3";
-            this.txtPeso3.Size = new System.Drawing.Size(66, 20);
-            this.txtPeso3.TabIndex = 6;
-            this.txtPeso3.Text = "200 P";
-            this.txtPeso3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.tiempofecha.Enabled = true;
             // 
-            // txtGramos1
+            // txtFecha
             // 
-            this.txtGramos1.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.txtGramos1.Location = new System.Drawing.Point(37, 99);
-            this.txtGramos1.Name = "txtGramos1";
-            this.txtGramos1.Size = new System.Drawing.Size(91, 28);
-            this.txtGramos1.TabIndex = 7;
-            this.txtGramos1.Text = "5 GRAMOS";
-            this.txtGramos1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.txtGramos1.Click += new System.EventHandler(this.TxtGramos1_Click);
-            // 
-            // textBox1
-            // 
-            this.textBox1.BackColor = System.Drawing.Color.Crimson;
-            this.textBox1.Font = new System.Drawing.Font("Verdana", 8.25F);
-            this.textBox1.ForeColor = System.Drawing.SystemColors.Info;
-            this.textBox1.Location = new System.Drawing.Point(451, 24);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(79, 21);
-            this.textBox1.TabIndex = 10;
-            this.textBox1.Text = "P = PUNTOS";
-            // 
-            // btn100gramos
-            // 
-            this.btn100gramos.Location = new System.Drawing.Point(270, 248);
-            this.btn100gramos.Name = "btn100gramos";
-            this.btn100gramos.Size = new System.Drawing.Size(22, 23);
-            this.btn100gramos.TabIndex = 12;
-            this.btn100gramos.Text = "✔";
-            this.btn100gramos.UseVisualStyleBackColor = false;
-            // 
-            // btn200gramos
-            // 
-            this.btn200gramos.Location = new System.Drawing.Point(415, 248);
-            this.btn200gramos.Name = "btn200gramos";
-            this.btn200gramos.Size = new System.Drawing.Size(22, 23);
-            this.btn200gramos.TabIndex = 13;
-            this.btn200gramos.Text = "✔";
-            this.btn200gramos.UseVisualStyleBackColor = false;
-            // 
-            // txtGramos2
-            // 
-            this.txtGramos2.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.txtGramos2.Location = new System.Drawing.Point(186, 99);
-            this.txtGramos2.Name = "txtGramos2";
-            this.txtGramos2.Size = new System.Drawing.Size(91, 28);
-            this.txtGramos2.TabIndex = 14;
-            this.txtGramos2.Text = "10 GRAMOS";
-            this.txtGramos2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // txtGramos3
-            // 
-            this.txtGramos3.BackColor = System.Drawing.SystemColors.ControlLight;
-            this.txtGramos3.Location = new System.Drawing.Point(340, 99);
-            this.txtGramos3.Name = "txtGramos3";
-            this.txtGramos3.Size = new System.Drawing.Size(91, 28);
-            this.txtGramos3.TabIndex = 15;
-            this.txtGramos3.Text = "20 GRAMOS";
-            this.txtGramos3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            // 
-            // btn5gramos
-            // 
-            this.btn5gramos.Location = new System.Drawing.Point(115, 248);
-            this.btn5gramos.Name = "btn5gramos";
-            this.btn5gramos.Size = new System.Drawing.Size(22, 23);
-            this.btn5gramos.TabIndex = 16;
-            this.btn5gramos.Text = "✔";
-            this.btn5gramos.UseVisualStyleBackColor = false;
+            this.txtFecha.AutoSize = true;
+            this.txtFecha.Font = new System.Drawing.Font("Verdana", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtFecha.Location = new System.Drawing.Point(603, 14);
+            this.txtFecha.Name = "txtFecha";
+            this.txtFecha.Size = new System.Drawing.Size(62, 18);
+            this.txtFecha.TabIndex = 6;
+            this.txtFecha.Text = "Fecha:";
             // 
             // frm_vidrio
             // 
@@ -193,27 +124,17 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.DarkGray;
             this.ClientSize = new System.Drawing.Size(800, 518);
-            this.Controls.Add(this.btn5gramos);
-            this.Controls.Add(this.txtGramos3);
-            this.Controls.Add(this.txtGramos2);
-            this.Controls.Add(this.btn200gramos);
-            this.Controls.Add(this.btn100gramos);
-            this.Controls.Add(this.textBox1);
-            this.Controls.Add(this.txtGramos1);
-            this.Controls.Add(this.txtPeso3);
-            this.Controls.Add(this.txtPeso2);
-            this.Controls.Add(this.txtPeso1);
-            this.Controls.Add(this.picPeso3);
-            this.Controls.Add(this.picPeso2);
-            this.Controls.Add(this.picPeso1);
+            this.Controls.Add(this.txtFecha);
+            this.Controls.Add(this.btn_salir);
+            this.Controls.Add(this.btn_siguiente);
+            this.Controls.Add(this.btn_Pesar);
+            this.Controls.Add(this.txtinformacion);
+            this.Controls.Add(this.txtInfo);
             this.Controls.Add(this.txtVidrio);
             this.Cursor = System.Windows.Forms.Cursors.Hand;
             this.Name = "frm_vidrio";
             this.Text = "frm_vidrio";
             this.Load += new System.EventHandler(this.Frm_vidrio_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.picPeso1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picPeso2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.picPeso3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -222,18 +143,12 @@
         #endregion
 
         private System.Windows.Forms.TextBox txtVidrio;
-        private System.Windows.Forms.PictureBox picPeso1;
-        private System.Windows.Forms.PictureBox picPeso2;
-        private System.Windows.Forms.PictureBox picPeso3;
-        private System.Windows.Forms.TextBox txtPeso1;
-        private System.Windows.Forms.TextBox txtPeso2;
-        private System.Windows.Forms.TextBox txtPeso3;
-        private System.Windows.Forms.Label txtGramos1;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Button btn100gramos;
-        private System.Windows.Forms.Button btn200gramos;
-        private System.Windows.Forms.Label txtGramos2;
-        private System.Windows.Forms.Label txtGramos3;
-        private System.Windows.Forms.Button btn5gramos;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label txtinformacion;
+        private System.Windows.Forms.Button btn_Pesar;
+        private System.Windows.Forms.Button btn_siguiente;
+        private System.Windows.Forms.Button btn_salir;
+        private System.Windows.Forms.Timer tiempofecha;
+        private System.Windows.Forms.Label txtFecha;
     }
 }
