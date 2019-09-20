1aa5e418d4831a68e1e8a092f9f6cf885befb53f
Diego Henderson <50886236+diegohenderson@users.noreply.github.com>
2019-09-19 22:16:05 -0300

    Carpeta Recursos + Fotos logo. y formulario para pesaje.

---
+++ Primaton-G3-Reciclaje/Primaton/Front/Formularios/frm_pesaje.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace Primaton.Front.Formularios
+{
+    partial class frm_pesaje
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frm_pesaje
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.WindowFrame;
+            this.ClientSize = new System.Drawing.Size(864, 548);
+            this.Name = "frm_pesaje";
+            this.Text = "frm_pesaje";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ Primaton-G3-Reciclaje/Primaton/Front/Formularios/frm_pesaje.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Primaton.Front.Formularios
+{
+    public partial class frm_pesaje : Form
+    {
+        public frm_pesaje()
+        {
+            InitializeComponent();
+        }
+    }
+}
