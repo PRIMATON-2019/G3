d70473613003f3a7e95fa0068a531879772d126a
Daiana Stefania Ramirez <d.s.ramirez@outlook.com>
2019-10-11 14:33:36 -0300

    Update frm_quienessomos.Designer.cs

--- Primaton-G3-Reciclaje/Primaton/Front/Formularios/frm_quienessomos.Designer.cs
+++ Primaton-G3-Reciclaje/Primaton/Front/Formularios/frm_quienessomos.Designer.cs
@@ -39,24 +39,25 @@
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.panel1.BackColor = System.Drawing.Color.White;
             this.panel1.Controls.Add(this.label1);
             this.panel1.Controls.Add(this.pictureBox1);
             this.panel1.Controls.Add(this.lbl_hora);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.panel1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(565, 543);
+            this.panel1.Size = new System.Drawing.Size(424, 441);
             this.panel1.TabIndex = 0;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(135, 286);
+            this.label1.Location = new System.Drawing.Point(101, 232);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(289, 25);
+            this.label1.Size = new System.Drawing.Size(235, 20);
             this.label1.TabIndex = 6;
             this.label1.Text = "informacion acerca de el grupoo";
             // 
@@ -66,35 +67,36 @@
             this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.pictureBox1.Cursor = System.Windows.Forms.Cursors.AppStarting;
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(3, 2);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Location = new System.Drawing.Point(2, 2);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(344, 170);
+            this.pictureBox1.Size = new System.Drawing.Size(258, 138);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
             // lbl_hora
             // 
-            this.lbl_hora.BackColor = System.Drawing.Color.MediumAquamarine;
+            this.lbl_hora.BackColor = System.Drawing.Color.White;
             this.lbl_hora.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
             this.lbl_hora.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
-            this.lbl_hora.ForeColor = System.Drawing.Color.DarkBlue;
-            this.lbl_hora.Location = new System.Drawing.Point(349, 0);
+            this.lbl_hora.ForeColor = System.Drawing.Color.Black;
+            this.lbl_hora.Location = new System.Drawing.Point(262, 0);
+            this.lbl_hora.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lbl_hora.Name = "lbl_hora";
-            this.lbl_hora.Size = new System.Drawing.Size(213, 173);
+            this.lbl_hora.Size = new System.Drawing.Size(160, 141);
             this.lbl_hora.TabIndex = 4;
             this.lbl_hora.Text = "12:32:25";
             this.lbl_hora.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // frm_quienessomos
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(565, 543);
+            this.ClientSize = new System.Drawing.Size(424, 441);
             this.Controls.Add(this.panel1);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "frm_quienessomos";
